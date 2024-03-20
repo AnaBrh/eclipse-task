790c4ec0b59773544b912eaf394cdaeef5b558b9
AnaBrh <anamariebrh@gmail.com>
2024-03-20 18:08:10 +0000

    created index file for easier imports

---
+++ utils/index.js
@@ -0,0 +1,18 @@
+export {
+	faCheck,
+	faHeart,
+	faChartSimple,
+} from "@fortawesome/free-solid-svg-icons";
+export { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export { fetchProducts } from "./api";
+export { default as ProductCard } from "../components/ProductCard";
+export { default as StarRating } from "../components/StarRating";
+export { default as StockStatus } from "../components/StockStatus";
+export { default as Timer } from "../components/Timer";
+export { default as capitalizeFirstLette } from "./capitaliseFirstLetter";
+export { default as useProductPrice } from "./useProductPrice";
+export { default as capitalizeFirstLetter } from "./capitaliseFirstLetter";
+export { default as AddToBasket } from "./AddToBasket";
+export { default as ProductFetcher } from "../components/ProductFetcher";
+export { default as ProductList } from "../components/ProductList";
+export { default as ProductImage } from "../components/ProductImage";
